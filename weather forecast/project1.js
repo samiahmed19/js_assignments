50d668c01d7f6d2a7e576fcc2251319bdd8857d7
Sami <119399293+samiahmed19@users.noreply.github.com>
2023-09-12 23:53:27 +0530

    Update project1.js

--- weather forecast/project1.js
+++ weather forecast/project1.js
@@ -1,29 +1,29 @@
-const weatherDataContainer = document.querySelector('.weather-data');
+const weatherData = document.querySelector('.weather-data');
 
-async function fetchWeatherData() {
+async function getWeatherData() {
   try {
     const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=84dbdbfeb8debd274ee06fbac1dc4124`);//calling the api key link of own apiKey
     if (url.ok) {
       const data = await url.json();
       const cityName = data.name;
       const temperature = data.main.temp;
-      const weatherDescription = data.weather[0].description;
+      const weatherDesc = data.weather[0].description;
 
       const htmlTemplate = `
         <h2>${cityName}</h2>
         <p>Temperature: ${temperature}°C</p>
-        <p>Weather: ${weatherDescription}</p>
+        <p>Weather: ${weatherDesc}</p>
       `;
 
-      weatherDataContainer.innerHTML = htmlTemplate;
+      weatherData.innerHTML = htmlTemplate;
     } 
     else {
-      weatherDataContainer.innerHTML = 'Failed to fetch weather data.';
+      weatherData.innerHTML = 'Failed to fetch weather data.';
     }
   } catch (error) {
     console.error('Error fetching weather data:', error);
-    weatherDataContainer.innerHTML = 'An error occurred while fetching weather data.';
+    weatherData.innerHTML = 'An error occurred while fetching weather data.';
   }
 }
 
-window.addEventListener('load', fetchWeatherData);
+window.addEventListener('load', getWeatherData);
